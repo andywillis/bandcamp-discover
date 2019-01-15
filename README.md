b9621a91ee86233522d306145792e244cd93206c
Andy Willis <github@awillis.fastmail.fm>
2019-01-15 00:25:32 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# bandcamp-rss
+
+Creates RSS feeds for Bandcamp.
+
+## Install
+
+`npm install`
+
+## Operation
+
+Either:
+
+1) Run `node wishlist [url1] [url2]`
+
+Or:
+
+1) Save a copy of the complete unrolled wishlist to the root folder as an HTML page and
+
+2) Run `node wishlist`
---
+++ index.js
@@ -0,0 +1,36 @@
+const request = require('request-promise-native');
+const cheerio = require('cheerio');
+
+const hour = 1000 * 60 * 60;
+
+function getMp3Links(html) {
+  const $ = cheerio.load(html);
+  const $item = $('.item');
+  const artRegex = /(https:\/\/.*\.jpg)/;
+  return $item.map((i, el) => {
+    return {
+      url: $(el).find('a').attr('href'),
+      art: $(el).html().match(artRegex)[1],
+      album: $(el).find('.itemtext').text(),
+      artist: $(el).find('.itemsubtext').text()
+    };
+  }).get();
+}
+
+async function getPageLinks(url) {
+  const html = await request(url);
+  return getMp3Links(html);
+}
+
+async function init(db) {
+  db = db || new Map();
+  const links = await getPageLinks('https://bandcamp.com/tag/electronic?page=2&sort_field=date');
+  links.forEach((link) => {
+    const { url } = link;
+    if (!db.has(url)) db.set(url, link);
+  });
+  console.log(`Database size: ${db.size}`);
+  // setTimeout(init, 30000, hour);
+}
+
+init();
